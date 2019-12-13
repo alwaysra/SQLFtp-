36e5af7782f663076627f20243f7c24ae429295e
alwaysra <alwaysra@163.com>
2019-12-13 11:48:35 +0800

    加入readme

--- SQLFtp备份/Program.cs
+++ SQLFtp备份/Program.cs
@@ -15,8 +15,8 @@ namespace SQLFtp备份
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
-            Application.Run(new Form1());
+            Application.Run(new Login());
+            //Application.Run(new Form1());
         }
     }
 }
